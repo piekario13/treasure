dba0b17c74ed73d33552447507003399e024a5e0
piekario13 <piekario13@gmail.com>
2018-08-31 23:39:03 +0200

    treasure

---
+++ main.js
@@ -0,0 +1,54 @@
+// $("html").mousemove(function (zdarzenie) {
+//     $("#title").offset({
+//         left: zdarzenie.pageX,
+//         top: zdarzenie.pageY
+//     });
+// });
+
+var uzyskajLosowąLiczbę = function (rozmiar) {
+    return Math.floor(Math.random() * rozmiar);
+};
+
+var obliczOdleglosc = function (zdarzenie, cel) {
+    var roznicaX = zdarzenie.offsetX - cel.x;
+    var roznicaY = zdarzenie.offset - cel.y;
+    return Math.sqrt((roznicaX * roznicaX) + (roznicaY * roznicaY));
+};
+
+var skonstruujPodpowiedz = function (odleglosc) {
+    if (odleglosc < 10) {
+        return "Parzy!";
+    } else if (odleglosc < 20) {
+        return "Gorąco!";
+    } else if (odleglosc < 40) {
+        return "Ciepło!";
+    } else if (odleglosc < 80) {
+        return "Letnio";
+    } else if (odleglosc < 160) {
+        return "Zimno";
+    } else if (odleglosc < 320) {
+        return "Mróz!";
+    } else {
+        return "Syberia!";
+    }
+};
+
+var szerokość = 400;
+var wysokość = 400;
+var klikniecia = 0;
+
+var cel = {
+    x: uzyskajLosowąLiczbę(szerokość),
+    y: uzyskajLosowąLiczbę(wysokość)
+};
+
+$("#mapa").click(function (zdarzenie) {
+    klikniecia++;
+    var odleglosc = obliczOdleglosc(zdarzenie, cel);
+    var podpowiedz = skonstruujPodpowiedz(odleglosc);
+    $("#odleglosc").text(podpowiedz);
+
+    if (odleglosc < 8) {
+    alert("Skarbzostał znaleziony po " + klikniecia + " kliknięciach!");
+    }
+})
